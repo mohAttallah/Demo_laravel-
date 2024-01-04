3d6f34b9754bc8f63b00a9361a11f994cd5e2933
mohAttallah <mohmohmoud28@gmail.com>
2024-01-04 00:29:35 +0000

    create data table

---
+++ app/Http/Controllers/select.php
@@ -0,0 +1,12 @@
+<?php
+
+namespace App\Http\Controllers;
+
+use Illuminate\Http\Request;
+
+class select extends Controller
+{
+    public function index (){
+        return view('welcome');
+    }
+}
